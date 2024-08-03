875bee42a43c3170e8a0c5bd13431e748cb0dddc
Dropxtor <121337501+Dropxtor@users.noreply.github.com>
2024-08-03 23:45:03 +0000

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "****************************************************"
+echo "#                                                  #"
+echo "#         Welcome to Dropxtor Script                    #"
+echo "#                                                  #"
+echo "****************************************************"
+
+sudo apt update -y
+sudo apt install screen -y
+screen -S farcaster
